a73c9a03aa7ca459eb600cc2fcf058357b0f80b6
Rahul Kumar <mailtorahulkrs@gmail.com>
2020-04-06 13:04:35 +0530

    CollectionDataBinding

---
+++ WPFDataBindingExample/WPFBinding Collection/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFBindingCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Person> Person { get; private set; }
+        public MainWindow()
+        {
+            Person = new ObservableCollection<Person>()
+            {
+                new Person(){Name="Rahul",LastName="Kumar",Age=25,PicturePath="C:\\Project-Result\\WPFDataBinding\\WPFDataBindingExample\\WPFBinding Collection\\Image\\Image1.jpg"},
+                new Person(){Name="Pankaj",LastName="Kumar",Age=28,PicturePath="C:\\Project-Result\\WPFDataBinding\\WPFDataBindingExample\\WPFBinding Collection\\Image\\Image2.jpg"}
+            };
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Person.Add(new Person
+            {
+                Name="Aashish",
+                LastName="Kumar",
+                Age=30,
+                PicturePath= "C:\\Project-Result\\WPFDataBinding\\WPFDataBindingExample\\WPFBinding Collection\\Image\\Image3.jpg"
+            });
+        }
+    }
+}
---
+++ WPFDataBindingExample/WPFBinding Collection/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBindingCollection
+{
+    public class Person:INotifyPropertyChanged
+    {
+        private string name;
+        public string Name {
+            get { return name; }
+                set{ name = value;
+                OnPropertyRaised(Name);
+            }
+            
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyRaised(LastName);
+            }
+        }
+
+        private int age;
+        public int Age {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyRaised("Age");
+            }
+        }
+
+        private string picturPath;
+        public string PicturePath {
+            get { return picturPath; }
+            set
+            {
+                picturPath = value;
+                OnPropertyRaised(PicturePath);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyRaised(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
