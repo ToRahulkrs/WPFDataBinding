099d47a50210271664e21088fbfdef4624b0740c
Rahul Kumar <mailtorahulkrs@gmail.com>
2020-04-07 17:18:31 +0530

    WPFApplicationModelInprogress

---
+++ WPFDataBindingExample/WPFApplictionModel/App.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Appliction = System.Windows.Application;
+
+namespace WPFApplictionModel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    ShutdownMode = ShutdownMode.OnLastWindowClose;
+        //    ShutdownMode = ShutdownMode.OnExplicitShutdown;
+        //    ShutdownMode = ShutdownMode.OnMainWindowClose;
+        //    base.OnStartup(e);
+        //}
+        //protected override void OnActivated(EventArgs e)
+        //{
+        //    base.OnActivated(e);
+        //}
+        //protected override void OnDeactivated(EventArgs e)
+        //{
+        //    base.OnDeactivated(e);
+        //}
+        //protected override void OnExit(ExitEventArgs e)
+        //{
+        //    base.OnExit(e);
+        //}
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DispatcherUnhandledException += MainUIThreadUnhandledException;
+            Current.Dispatcher.UnhandledException += DispatcherOnUnhandledException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerExceptionOnUnobservedTaskException;
+        }
+
+        #region
+        private void TaskSchedulerExceptionOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Debug.WriteLine("TaskSchedulerExceptionOnUnobservedTaskException");
+        }
+
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Debug.WriteLine("CurrentDomainOnUnhandledException");
+        }
+
+        private void DispatcherOnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Debug.WriteLine("DispatcherOnUnhandledException");
+            e.Handled = true;
+        }
+
+        private void MainUIThreadUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Debug.WriteLine("MainUIThreadUnhandledException");
+            e.Handled = true;
+        }
+        #endregion
+    }
+}
---
+++ WPFDataBindingExample/WPFApplictionModel/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplictionModel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ThrowException()
+        {
+            throw new Exception();
+        }
+        #region
+        //protected override void OnActivated(EventArgs e)
+        //{
+        //    base.OnActivated(e);
+        //}
+        //protected override void OnContentRendered(EventArgs e)
+        //{
+        //    base.OnContentRendered(e);
+        //}
+        //protected override void OnClosing(CancelEventArgs e)
+        //{
+        //    e.Cancel = true;
+        //    base.OnClosing(e);
+        //}
+        //protected override void OnDeactivated(EventArgs e)
+        //{
+        //    base.OnDeactivated(e);
+        //}
+        //protected override void OnClosed(EventArgs e)
+        //{
+        //    base.OnClosed(e);
+        //}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            //Task.Run(() =>
+            //{
+            //    Application.Current.Dispatcher.Invoke(() =>
+            //    {
+            //        Start.Content = "Stop";
+
+            //    });
+            //});
+            //Thread.Sleep(10000);
+            Thread t = new Thread(ThrowException);
+            t.Start();
+
+            //ThrowException();
+        }
+    }
+}
