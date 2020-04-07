15cedf39cb11789fde2cbde49244ef3adf4f69c1
Rahul Kumar <mailtorahulkrs@gmail.com>
2020-04-07 15:32:16 +0530

    UserConterlExampleInprogress

---
+++ WPFDataBindingExample/UserControlExample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<PersonInfo> personInfos;
+        public MainWindow(ObservableCollection<PersonInfo> personInfos)
+        {
+            this.personInfos = personInfos;
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            personInfos.Add(new PersonInfo {Name=Person.Name,LastName=Person.LastName,Age=int.Parse(Person.Age) });
+            Close();
+        }
+    }
+}
---
+++ WPFDataBindingExample/UserControlExample/Person.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlExample
+{
+    /// <summary>
+    /// Interaction logic for Person.xaml
+    /// </summary>
+    public partial class Person : UserControl
+    {
+        public Person()
+        {
+            InitializeComponent();
+        }
+
+        public string Age { get { return AgeBox.Text; }  }
+        public string Name { get { return NameBox.Text; } }
+        public string LastName { get { return LastNameBox.Text; } }
+
+    }
+}
